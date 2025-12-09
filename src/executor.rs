a8b4220d5da010fe1ba807afd279d0c354e5c1c4
marc28092005-lgtm <marc28092005@gmail.com>
2025-12-09 21:07:52 +0100

    Update executor.rs

--- src/executor.rs
+++ src/executor.rs
@@ -1,23 +1,25 @@
-mod storage;
-use crate::executor::storage::*;
-use std::collections::HashMap;
+
+use crate::storage;
 use std::io::{self, Write};
 
+use serde::{Serialize, Deserialize};
+use crate::storage::load;
+
+#[derive(Serialize, Deserialize)]
 struct CustomerValue {
     sales_volume: i32,
-    newsletter  : bool
+    newsletter: bool,
 }
 
+#[derive(Serialize, Deserialize)]
 enum EnCustomer {
-    Status(CustomerValue)
+    Status(CustomerValue),
 }
 
 pub fn start() {
-    let mut customer_map : HashMap<String, EnCustomer> = HashMap::new();
-    let mut maps_list: Vec<HashMap<String, EnCustomer>> = Vec::new();
+    let mut map = load();
 
     loop {
-
         // Das fertige Programm soll ¨uber eine Kommandozeilenschnittstelle steuerbar sein
         // und grundlegende Operationen wie INSERT, SELECT, DELETE und UPDATE ermöglichen
         let mut eingabe = String::new();
@@ -27,18 +29,21 @@ pub fn start() {
         println!("Löschen        / DELETE   < d >");
         println!("Aktualisieren  / UPDATE   < u >");
         println!("Beenden        / END      < x >");
-        print!  ("Bitte Auswahl treffen     : ");
-        io::stdout().flush().unwrap();
-        io::stdin().read_line(&mut eingabe).unwrap();
+        print!("Bitte Auswahl treffen: ");
+        std::io::stdout().flush().unwrap();
+        std::io::stdin().read_line(&mut eingabe).unwrap();
+
         match eingabe.trim() {
-            "i" | "I" => storage::insert(),
-            "s" | "S" => storage::select(),
-            "d" | "D" => storage::delete(),
-            "u" | "U" => storage::update(),
-            "x" | "X" => { break },
+            "i" | "I" => storage::insert(&mut map),
+            "s" | "S" => storage::select(&map),
+            "d" | "D" => storage::delete(&mut map),
+            "u" | "U" => storage::update(&mut map),
+            "x" | "X" => {
+                storage::save(&map);
+                break;
+            },
             _ => continue,
         }
-
     }
 }
 
